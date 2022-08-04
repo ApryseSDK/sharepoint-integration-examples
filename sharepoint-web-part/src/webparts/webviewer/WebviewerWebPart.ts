72f523478b42bdec0156ebb96c596399c70a39cd
Johnny Kung <jkung@pdftron.com>
2022-08-04 11:59:11 -0700

    * Changed the path of webviewer to use static assets inside sharepoint   documents
    * Added save file function to the sharepoint-web-parts webviewr

--- sharepoint-web-part/src/webparts/webviewer/WebviewerWebPart.ts
+++ sharepoint-web-part/src/webparts/webviewer/WebviewerWebPart.ts
@@ -5,7 +5,7 @@ import {
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
-import WebViewer from '@pdftron/webviewer';
+import WebViewer, { Core, UI, WebViewerInstance } from '@pdftron/webviewer';
 
 import * as strings from 'WebviewerWebPartStrings';
 
@@ -22,7 +22,7 @@ export default class WebviewerWebPart extends BaseClientSideWebPart<IWebviewerWe
   
   public validateQueryParam(urlParams: URLSearchParams): boolean {
     const necessaryParams: string[] = ['uniqueId', 'tempAuth', 'filename'];
-    let result = true;
+    let result: boolean = true;
     necessaryParams.forEach(paramKey => {
       if (!urlParams.get(paramKey)) {
         result = false;
@@ -35,12 +35,15 @@ export default class WebviewerWebPart extends BaseClientSideWebPart<IWebviewerWe
     this.domElement.style.height = '1000px';
 
     WebViewer({
-      path: '/_catalogs/masterpage/pdftron/lib',
-      uiPath: './ui/index.aspx'
+      // path: '/_catalogs/masterpage/pdftron/lib',
+      path: 'https://5s4vrg.sharepoint.com/sites/5s4vrg/Shared Documents/test-site/sharepoint-static/js/lib',
+      uiPath: './ui/index.aspx',
     }, this.domElement)
     .then(instance => {
       const urlParams: URLSearchParams = new URLSearchParams(window.location.search);
       const validateQueryParamResult: boolean = this.validateQueryParam(urlParams);
+      this._createSavedModal(instance);
+      this._createSaveFileButton(instance);
       if (validateQueryParamResult) {
         const uniqueId: string = urlParams.get("uniqueId");
         const tempAuth: string = urlParams.get("tempAuth");
@@ -56,6 +59,102 @@ export default class WebviewerWebPart extends BaseClientSideWebPart<IWebviewerWe
       }
     })
     .catch(err => console.log(err));
+  }
+
+  private _createSaveFileButton(instance: WebViewerInstance): void {
+    const me = this;
+    instance.UI.setHeaderItems(function(header: UI.Header) {
+      const saveFileButton: unknown = {
+        type: 'actionButton',
+        dataElement: 'saveFileButton',
+        title: 'Save file to sharepoint',
+        img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
+        onClick: async function() {
+          const searchparams: URLSearchParams = new URLSearchParams(window.location.search);
+          const folderName: string = searchparams.get('foldername');
+          const fileName: string = searchparams.get('filename');
+          instance.UI.openElements(['loadingModal']);
+          await me.saveFile(instance, folderName, fileName);
+          instance.UI.closeElements(['loadingModal']);
+          instance.UI.openElements(['savedModal']);
+        }
+      };
+      header.get('viewControlsButton').insertBefore(saveFileButton);
+    })
+  }
+
+  private async _getFormDigestValue(): Promise<string> {
+    try {
+      const resp: Response = await fetch(`${window.location.origin}/sites/5s4vrg/_api/contextinfo`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json; odata=verbose'
+        },
+      });
+      
+      interface IDigestResponseJson {
+        d: {
+          GetContextWebInformation: {
+            FormDigestValue: string
+          }
+        }
+      }
+      const respJson: IDigestResponseJson = await resp.json();
+      return respJson.d.GetContextWebInformation.FormDigestValue;
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
+  public async saveFile(instance: WebViewerInstance, folderUrl: string, fileName: string): Promise<void> {
+    const annotationManager: Core.AnnotationManager = instance.Core.annotationManager;
+    const xfdfString: string = await annotationManager.exportAnnotations();
+    const fileData: ArrayBuffer = await instance.Core.documentViewer.getDocument().getFileData({ xfdfString });
+    const digest: string = await this._getFormDigestValue();
+    
+    const fileArray: Uint8Array= new Uint8Array(fileData);
+    const file: File = new File([fileArray], fileName, {
+      type: 'application/pdf'
+    });
+    await fetch(`${window.location.origin}/sites/5s4vrg/_api/web/GetFolderByServerRelativeUrl('${folderUrl}')/Files/add(url='${fileName}', overwrite=true)`, {
+      method: 'POST',
+      body: file,
+      headers: {
+        'accept': 'application/json; odata=verbose',
+        'X-RequestDigest': digest,
+        'Content-Length': fileData.byteLength.toString()
+      }
+    });
+  }
+
+  private _createSavedModal(instance: WebViewerInstance): void {
+    const divInput: HTMLElement = document.createElement('div');
+    divInput.innerText = 'File saved successfully';
+
+    interface IModal { 
+      dataElement: string;
+      disableBackdropClick?: boolean; 
+      disableEscapeKeyDown?: boolean; 
+      render: UI.renderCustomModal; 
+      header: unknown; 
+      body: unknown; 
+      footer: unknown; 
+    }
+
+    const modal: IModal = {
+      dataElement: 'savedModal',
+      body: {
+        className: 'myCustomModal-body',
+        style: {
+          'text-align': 'center'
+        },
+        children: [divInput]
+      },
+      header: null,
+      footer: null,
+      render: null
+    }
+    instance.UI.addCustomModal(modal);
   }
 
   protected onInit(): Promise<void> {
